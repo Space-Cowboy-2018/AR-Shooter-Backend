fd623adcb4d0776c388c6cbcfdf5d577055a0ee2
Man Jun (John) Hui <mjhui90@gmail.com>
2018-06-21 19:21:43 -0400

    Socket emits position #4 (#5)
    * feat(server): added shot emit
    
    * feat(server): broadcast socket

--- server.js
+++ server.js
@@ -11,19 +11,31 @@ app.use(express.urlencoded({ extended: true }));
 
 const server = http.createServer();
 server.listen(PORT, () => {
-  console.log('listening');
+  console.log('listening on', PORT);
 });
 server.on('request', app);
 const io = socketio(server);
 
 io.on('connection', socket => {
-
+  let count = 0;
   console.log('A new client has connected!');
   console.log(socket.id);
 
-  socket.on('position', ({position, aim}) => {
-    console.log('Id: ', socket.id)
-    console.log('position: ', position);
-    console.log('aim: ', aim);
-  })
-})
+  socket.on('position', ({ position, aim }) => {
+    console.log('Id: ', socket.id);
+    console.log('shooters position: ', position);
+
+    socket.broadcast.emit('shot', position);
+
+  });
+  socket.on('gothit?', targetPlayer => {
+    console.log('targets position', targetPlayer);
+  });
+
+  socket.on('disconnect', function() {
+    console.log('i disconnected~');
+    socket.removeAllListeners('gothit?');
+    socket.removeAllListeners('position');
+    socket.removeAllListeners('disconnect');
+  });
+});
