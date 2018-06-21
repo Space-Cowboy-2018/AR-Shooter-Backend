c590ada6c91a80cedebeff88e0bd67ec74717255
Isaias Pomales <impomales@gmail.com>
2018-06-21 14:25:55 -0400

    fix(bug): miscalculated vectors

--- math.js
+++ math.js
@@ -5,28 +5,31 @@ const BUFFER = 0.05;
 // direction is camera.getWorldDirection of shooter.
 function isHit(shooter, target, direction) {
   // targetX - shooterX
-  const deltaX = target.position.x - shooter.position.x;
+  const deltaX = target.x - shooter.x;
   // shooterZ - targetZ
-  const deltaZ = shooter.position.z - target.position.z;
+  const deltaZ = shooter.z - target.z;
   // angle.
   const theta =
-    direction.z < 0 ? -Math.atan(deltaX / deltaZ) : Math.atan(deltaX / deltaZ);
+    direction.z < 0
+      ? -Math.atan(direction.x / direction.z)
+      : Math.atan(direction.x / direction.z);
   // pythagorean thm.
   const vector = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaZ, 2));
   // x component of vector Vsin(theta)
   const vectorX = vector * Math.sin(theta);
-  const vectorZ = direction.z < 0 ? -Math.cos(theta) : Math.cos(theta);
+  const vectorZ =
+    direction.z < 0 ? -Math.cos(theta) * vector : Math.cos(theta) * vector;
   // destination.
   const destination = {
-    x: shooter.position.x + vectorX,
-    z: shooter.position.z + vectorZ
+    x: shooter.x + vectorX,
+    z: shooter.z + vectorZ
   };
   const buffer = {
-    x: Math.abs(target.position.x - destination.x),
-    z: Math.abs(destination.z - target.position.z)
+    x: Math.abs(target.x - destination.x),
+    z: Math.abs(destination.z - target.z)
   };
   // test if destination is close enough to target.
   return buffer.x < BUFFER && buffer.z < BUFFER;
-};
+}
 
-module.exports = { isHit }
+module.exports = { isHit };
