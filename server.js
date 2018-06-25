096d14db970d12e946ae286da5c677daa72983a0
richardsunny2015 <richardsunny2015@gmail.com>
2018-06-25 15:54:02 -0400

    feat(server): created route to send rooms

--- server.js
+++ server.js
@@ -1,56 +1,62 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
 const app = express();
 const PORT = process.env.PORT || 3030;
-const { isHit } = require('./math.js');
+const { isHit } = require("./math.js");
 let shooterPosition, shooterAim;
-let rooms = {};
+let rooms = {
+  cow: 1,
+  chicken: 1,
+  moose: 1
+};
 
 // Parse HTTP JSON bodies
 app.use(express.json());
 // Parse URL-encoded params
 app.use(express.urlencoded({ extended: true }));
+app.get("/rooms", (req, res, next) => {
+  res.json(rooms);
+});
 
 const server = http.Server(app);
 server.listen(PORT);
-server.on('request', app);
+server.on("request", app);
 const io = socketio(server);
 
-io.on('connection', socket => {
-  let count = 0;
-  let ourRoom = '';
-  console.log('A new client has connected!', socket.id);
-  socket.on('createRoom', name => {
+io.on("connection", socket => {
+  let ourRoom = "";
+  console.log("A new client has connected!", socket.id);
+  socket.on("createRoom", name => {
     ourRoom = name;
-    console.log('this is our room name>>>>>', name);
+    console.log("this is our room name>>>>>", name);
     socket.join(name);
     rooms[name] = 1;
-    socket.emit('createdRoom', rooms);
-    console.log('createdRoom!!!');
+    socket.emit("createdRoom", rooms);
+    console.log("createdRoom!!!");
   });
-  socket.on('joinRoom', name => {
+  socket.on("joinRoom", name => {
     ourRoom = name;
     socket.join(name);
-    console.log('this is our room name on join>>>>', name);
+    console.log("this is our room name on join>>>>", name);
     rooms[name]++;
   });
-  socket.on('position', ({ position, aim }) => {
-    console.log('rooms', ourRoom);
+  socket.on("position", ({ position, aim }) => {
+    console.log("rooms", ourRoom);
     shooterPosition = position;
     shooterAim = aim;
-    socket.to(ourRoom).emit('shot', position);
+    socket.to(ourRoom).emit("shot", position);
   });
-  socket.on('gothit?', targetPlayer => {
+  socket.on("gothit?", targetPlayer => {
     console.log(socket.rooms);
     const didWeHit = isHit(shooterPosition, targetPlayer, shooterAim);
-    console.log('DID WE HIT?>>>>' + didWeHit);
+    console.log("DID WE HIT?>>>>" + didWeHit);
   });
 
-  socket.on('disconnect', function() {
-    console.log('i disconnected~', socket.id);
-    socket.removeAllListeners('gothit?');
-    socket.removeAllListeners('position');
-    socket.removeAllListeners('disconnect');
+  socket.on("disconnect", function() {
+    console.log("i disconnected~", socket.id);
+    socket.removeAllListeners("gothit?");
+    socket.removeAllListeners("position");
+    socket.removeAllListeners("disconnect");
   });
 });
