20c739c03c648861cea3c1f0202e326ed6bc1f3d
richardsunny2015 <richardsunny2015@gmail.com>
2018-06-26 17:47:15 -0400

    feat(reducer): reducer has add_room action

--- store/actions.js
+++ store/actions.js
@@ -1,1 +1,11 @@
-const ADD_ROOM
+// ACTIONS
+const ADD_ROOM = 'ADD_ROOM';
+
+// ACTION CREATORS
+const addRoom = (roomName, initialPlayer) => ({
+  type: ADD_ROOM,
+  roomName,
+  initialPlayer
+});
+
+module.exports = { ADD_ROOM, addRoom };
--- store/reducer.js
+++ store/reducer.js
@@ -1,3 +1,14 @@
-const reducer = (state = {}, action) => {};
+const { ADD_ROOM } = require('./actions');
+
+const reducer = (state = {}, action) => {
+  const stateCopy = { ...state };
+  switch (action.type) {
+    case ADD_ROOM:
+      stateCopy[action.roomName] = [action.initialPlayer];
+      return stateCopy;
+    default:
+      return state;
+  }
+};
 
 module.exports = reducer;
