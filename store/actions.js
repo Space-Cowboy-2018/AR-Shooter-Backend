53b901ebb93837c6fbb3b2cb9f4be510ac65489a
Richard Lanchez <richardsunny2015@gmail.com>
2018-06-27 11:03:45 -0400

    feat(store): store now has add_player action (#48)

--- store/actions.js
+++ store/actions.js
@@ -1,5 +1,6 @@
 // ACTIONS
 const ADD_ROOM = 'ADD_ROOM';
+const ADD_PLAYER_TO_ROOM = 'ADD_PLAYER_TO_ROOM';
 
 // ACTION CREATORS
 const addRoom = (roomName, initialPlayer) => ({
@@ -8,4 +9,9 @@ const addRoom = (roomName, initialPlayer) => ({
   initialPlayer
 });
 
-module.exports = { ADD_ROOM, addRoom };
+const addPlayerToRoom = (roomName, player) => ({
+  type: ADD_PLAYER_TO_ROOM,
+  roomName,
+  player
+});
+module.exports = { ADD_ROOM, addRoom, ADD_PLAYER_TO_ROOM, addPlayerToRoom };
--- store/reducer.js
+++ store/reducer.js
@@ -1,11 +1,18 @@
-const { ADD_ROOM } = require('./actions');
+const { ADD_ROOM, ADD_PLAYER_TO_ROOM } = require('./actions');
 
 const reducer = (state = {}, action) => {
   const stateCopy = { ...state };
   switch (action.type) {
     case ADD_ROOM:
-      stateCopy[action.roomName] = 1;
+      stateCopy[action.roomName] = [action.initialPlayer];
       return stateCopy;
+    case ADD_PLAYER_TO_ROOM:
+      stateCopy[action.roomName] = [
+        ...stateCopy[action.roomName],
+        action.player
+      ];
+      return stateCopy;
+
     default:
       return state;
   }
