2943f030638208d0bb5e3ec56318f318d2f903c4
Isaias Pomales <impomales@gmail.com>
2018-07-02 11:29:21 -0400

    fix(tests): remove tests taking y axis into account (#78)

--- utils/math.spec.js
+++ utils/math.spec.js
@@ -14,7 +14,7 @@ describe('isHit function', () => {
     it('can hit target at 45 degree angle', () => {
       const shooter = { x: 0, y: 0, z: 0 };
       const target = { x: 1, y: 0, z: -1 };
-      const aim = { x: 0.707106781186548, y: 0, z: 0.707106781186548 };
+      const aim = { x: 0.707106781186548, y: 0, z: -0.707106781186548 };
 
       expect(isHit(shooter, target, aim)).to.equal(true);
     });
@@ -23,30 +23,6 @@ describe('isHit function', () => {
       const shooter = { x: 0, y: 0, z: 0 };
       const target = { x: 0, y: 0, z: 1 };
       const aim = { x: 0, y: 0, z: 1 };
-
-      expect(isHit(shooter, target, aim)).to.equal(true);
-    });
-
-    it('can hit target at 45 degree angle with y above', () => {
-      const shooter = { x: 0, y: 0, z: 0 };
-      const target = { x: 1, y: 1, z: -1 };
-      const aim = {
-        x: 0.707106781186548,
-        y: 0.707106781186548,
-        z: 0.707106781186548
-      };
-
-      expect(isHit(shooter, target, aim)).to.equal(true);
-    });
-
-    it('can hit target at 45 degree angle with y below', () => {
-      const shooter = { x: 0, y: 0, z: 0 };
-      const target = { x: 1, y: -1, z: -1 };
-      const aim = {
-        x: 0.707106781186548,
-        y: -0.707106781186548,
-        z: 0.707106781186548
-      };
 
       expect(isHit(shooter, target, aim)).to.equal(true);
     });
@@ -78,7 +54,7 @@ describe('isHit function', () => {
 
     it('can hit target at 45 degree angle', () => {
       const target = { x: 1, y: 0, z: -2 };
-      const aim = { x: 0.707106781186548, y: 0, z: 0.707106781186548 };
+      const aim = { x: 0.707106781186548, y: 0, z: -0.707106781186548 };
 
       expect(isHit(shooter, target, aim)).to.equal(true);
     });
@@ -86,28 +62,6 @@ describe('isHit function', () => {
     it('can hit target from the back', () => {
       const target = { x: 0, y: 0, z: 2 };
       const aim = { x: 0, y: 0, z: 1 };
-
-      expect(isHit(shooter, target, aim)).to.equal(true);
-    });
-
-    it('can hit target at 45 degree angle with y above', () => {
-      const target = { x: 1, y: 1, z: -2 };
-      const aim = {
-        x: 0.707106781186548,
-        y: 0.707106781186548,
-        z: 0.707106781186548
-      };
-
-      expect(isHit(shooter, target, aim)).to.equal(true);
-    });
-
-    it('can hit target at 45 degree angle with y below', () => {
-      const target = { x: 1, y: -1, z: -2 };
-      const aim = {
-        x: 0.707106781186548,
-        y: -0.707106781186548,
-        z: 0.707106781186548
-      };
 
       expect(isHit(shooter, target, aim)).to.equal(true);
     });
